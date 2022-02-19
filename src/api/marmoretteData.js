ede143fed84957976855776a656c10573a6aea99
Exerbaind <muchapm@yandex.ru>
2022-02-19 22:15:24 +0300

    new collection

---
+++ src/api/marmoretteData.js
@@ -0,0 +1,463 @@
+function MarmoretteArticles() {
+    return [
+      {
+        id: 0,
+        name: "Dark Brown",
+        number: "0003",
+        img: "./assets/images/collections/marmorette/articles/0003.jpeg",
+      },
+      {
+        id: 1,
+        name: "Lobster Red",
+        number: "0018",
+        img: "./assets/images/collections/marmorette/articles/0018.jpeg",
+      },
+      {
+        id: 2,
+        name: "Sunset Orange",
+        number: "0019",
+        img: "./assets/images/collections/marmorette/articles/0019.jpeg",
+      },
+      {
+        id: 3,
+        name: "Autumn Blue",
+        number: "0022",
+        img: "./assets/images/collections/marmorette/articles/0022.jpeg",
+      },
+      {
+        id: 4,
+        name: "Dusty Blue",
+        number: "0023",
+        img: "./assets/images/collections/marmorette/articles/0023.jpeg",
+      },
+      {
+        id: 5,
+        name: "Sky Blue",
+        number: "0026",
+        img: "./assets/images/collections/marmorette/articles/0026.jpeg",
+      },
+      {
+        id: 6,
+        name: "Dive",
+        number: "0028",
+        img: "./assets/images/collections/marmorette/articles/0028.jpeg",
+      },
+      {
+        id: 7,
+        name: "Light Sahara",
+        number: "0040",
+        img: "./assets/images/collections/marmorette/articles/0040.jpeg",
+      },
+      {
+        id: 8,
+        name: "Shortcrust",
+        number: "0042",
+        img: "./assets/images/collections/marmorette/articles/0042.jpeg",
+      },
+      {
+        id: 9,
+        name: "Leaf Green",
+        number: "0043",
+        img: "./assets/images/collections/marmorette/articles/0043.jpeg",
+      },
+      {
+        id: 10,
+        name: "Sand Beige",
+        number: "0045",
+        img: "./assets/images/collections/marmorette/articles/0045.jpeg",
+      },
+      {
+        id: 11,
+        name: "Royal Blue",
+        number: "0049",
+        img: "./assets/images/collections/marmorette/articles/0049.jpeg",
+      },
+      {
+        id: 12,
+        name: "Quartz Grey",
+        number: "0050",
+        img: "./assets/images/collections/marmorette/articles/0050.jpeg",
+      },
+      {
+        id: 13,
+        name: "Ice Grey",
+        number: "0053",
+        img: "./assets/images/collections/marmorette/articles/0053.jpeg",
+      },
+      {
+        id: 14,
+        name: "Concrete Patty",
+        number: "0054",
+        img: "./assets/images/collections/marmorette/articles/0054.jpeg",
+      },
+      {
+        id: 15,
+        name: "Ash Grey",
+        number: "0055",
+        img: "./assets/images/collections/marmorette/articles/0055.jpeg",
+      },
+      {
+        id: 16,
+        name: "Foggy Blue",
+        number: "0056",
+        img: "./assets/images/collections/marmorette/articles/0056.jpeg",
+      },
+      {
+        id: 17,
+        name: "Plumb Grey",
+        number: "0059",
+        img: "./assets/images/collections/marmorette/articles/0059.jpeg",
+      },
+      {
+        id: 18,
+        name: "Rocky Brown",
+        number: "0070",
+        img: "./assets/images/collections/marmorette/articles/0070.jpeg",
+      },
+      {
+        id: 19,
+        name: "Golden Yellow",
+        number: "0072",
+        img: "./assets/images/collections/marmorette/articles/0072.jpeg",
+      },
+      {
+        id: 20,
+        name: "Spicy Orange",
+        number: "0073",
+        img: "./assets/images/collections/marmorette/articles/0073.jpeg",
+      },
+      {
+        id: 21,
+        name: "Light Aurora",
+        number: "0075",
+        img: "./assets/images/collections/marmorette/articles/0075.jpeg",
+      },
+      {
+        id: 22,
+        name: "Pale Yellow",
+        number: "0076",
+        img: "./assets/images/collections/marmorette/articles/0076.jpeg",
+      },
+      {
+        id: 23,
+        name: "Jungle",
+        number: "0083",
+        img: "./assets/images/collections/marmorette/articles/0083.jpeg",
+      },
+      {
+        id: 24,
+        name: "Peacan Pie",
+        number: "0089",
+        img: "./assets/images/collections/marmorette/articles/0089.jpeg",
+      },
+      {
+        id: 25,
+        name: "Soapstone",
+        number: "0090",
+        img: "./assets/images/collections/marmorette/articles/0090.jpeg",
+      },
+      {
+        id: 26,
+        name: "Shiitake Mushroom",
+        number: "0091",
+        img: "./assets/images/collections/marmorette/articles/0091.jpeg",
+      },
+      {
+        id: 27,
+        name: "Beach",
+        number: "0092",
+        img: "./assets/images/collections/marmorette/articles/0092.jpeg",
+      },
+      {
+        id: 28,
+        name: "Midnight Grey",
+        number: "0096",
+        img: "./assets/images/collections/marmorette/articles/0096.jpeg",
+      },
+      {
+        id: 29,
+        name: "Desert Beige",
+        number: "0098",
+        img: "./assets/images/collections/marmorette/articles/0098.jpeg",
+      },
+      {
+        id: 30,
+        name: "Grey Turquoise",
+        number: "0099",
+        img: "./assets/images/collections/marmorette/articles/0099.jpeg",
+      },
+      {
+        id: 31,
+        name: "Frog Green",
+        number: "0100",
+        img: "./assets/images/collections/marmorette/articles/0100.jpeg",
+      },
+      {
+        id: 32,
+        name: "Hazelnut Shake",
+        number: "0106",
+        img: "./assets/images/collections/marmorette/articles/0106.jpeg",
+      },
+      {
+        id: 33,
+        name: "Mokka Brown",
+        number: "0108",
+        img: "./assets/images/collections/marmorette/articles/0108.jpeg",
+      },
+      {
+        id: 34,
+        name: "Mandarin Orange",
+        number: "0117",
+        img: "./assets/images/collections/marmorette/articles/0117.jpeg",
+      },
+      {
+        id: 35,
+        name: "Chili Red",
+        number: "0118",
+        img: "./assets/images/collections/marmorette/articles/0118.jpeg",
+      },
+      {
+        id: 36,
+        name: "Terracotta",
+        number: "0119",
+        img: "./assets/images/collections/marmorette/articles/0119.jpeg",
+      },
+      {
+        id: 37,
+        name: "Blue Eyes",
+        number: "0121",
+        img: "./assets/images/collections/marmorette/articles/0121.jpeg",
+      },
+      {
+        id: 38,
+        name: "Fluffy Blue",
+        number: "0122",
+        img: "./assets/images/collections/marmorette/articles/0122.jpeg",
+      },
+      {
+        id: 39,
+        name: "Violet",
+        number: "0128",
+        img: "./assets/images/collections/marmorette/articles/0128.jpeg",
+      },
+      {
+        id: 40,
+        name: "Curacao Petrol",
+        number: "0129",
+        img: "./assets/images/collections/marmorette/articles/0129.jpeg",
+      },
+      {
+        id: 41,
+        name: "Antique Green",
+        number: "0130",
+        img: "./assets/images/collections/marmorette/articles/0130.jpeg",
+      },
+      {
+        id: 42,
+        name: "Lime Green",
+        number: "0132",
+        img: "./assets/images/collections/marmorette/articles/0132.jpeg",
+      },
+      {
+        id: 43,
+        name: "Turquise Bay",
+        number: "0136",
+        img: "./assets/images/collections/marmorette/articles/0136.jpeg",
+      },
+      {
+        id: 44,
+        name: "Khaki",
+        number: "0138",
+        img: "./assets/images/collections/marmorette/articles/0138.jpeg",
+      },
+      {
+        id: 45,
+        name: "Banana White",
+        number: "0145",
+        img: "./assets/images/collections/marmorette/articles/0145.jpeg",
+      },
+      {
+        id: 46,
+        name: "Beeswax Beige",
+        number: "0146",
+        img: "./assets/images/collections/marmorette/articles/0146.jpeg",
+      },
+      {
+        id: 47,
+        name: "Ink Blue",
+        number: "0148",
+        img: "./assets/images/collections/marmorette/articles/0148.jpeg",
+      },
+      {
+        id: 48,
+        name: "Dark Blue",
+        number: "0149",
+        img: "./assets/images/collections/marmorette/articles/0149.jpeg",
+      },
+      {
+        id: 49,
+        name: "Greystone Grey",
+        number: "0153",
+        img: "./assets/images/collections/marmorette/articles/0153.jpeg",
+      },
+      {
+        id: 50,
+        name: "Smoked Pearl",
+        number: "0155",
+        img: "./assets/images/collections/marmorette/articles/0155.jpeg",
+      },
+      {
+        id: 51,
+        name: "Tabac Grey",
+        number: "0158",
+        img: "./assets/images/collections/marmorette/articles/0158.jpeg",
+      },
+      {
+        id: 52,
+        name: "Industrial Grey",
+        number: "0160",
+        img: "./assets/images/collections/marmorette/articles/0160.jpeg",
+      },
+      {
+        id: 53,
+        name: "Milk Coffee",
+        number: "0164",
+        img: "./assets/images/collections/marmorette/articles/0164.jpeg",
+      },
+      {
+        id: 54,
+        name: "Clay",
+        number: "0166",
+        img: "./assets/images/collections/marmorette/articles/0166.jpeg",
+      },
+      {
+        id: 55,
+        name: "Papaya Orange",
+        number: "0172",
+        img: "./assets/images/collections/marmorette/articles/0172.jpeg",
+      },
+      {
+        id: 56,
+        name: "Melon Orange",
+        number: "0173",
+        img: "./assets/images/collections/marmorette/articles/0173.jpeg",
+      },
+      {
+        id: 57,
+        name: "Pink",
+        number: "0211",
+        img: "./assets/images/collections/marmorette/articles/0211.jpeg",
+      },
+      {
+        id: 58,
+        name: "Mystery Blue",
+        number: "0224",
+        img: "./assets/images/collections/marmorette/articles/0224.jpeg",
+      },
+      {
+        id: 59,
+        name: "Marble Beige",
+        number: "0243",
+        img: "./assets/images/collections/marmorette/articles/0243.jpeg",
+      },
+      {
+        id: 60,
+        name: "Cream",
+        number: "0251",
+        img: "./assets/images/collections/marmorette/articles/0251.jpeg",
+      },
+      {
+        id: 61,
+        name: "Light Grey",
+        number: "0252",
+        img: "./assets/images/collections/marmorette/articles/0252.jpeg",
+      },
+      {
+        id: 62,
+        name: "Pebble Grey",
+        number: "0253",
+        img: "./assets/images/collections/marmorette/articles/0253.jpeg",
+      },
+      {
+        id: 63,
+        name: "Mineral Grey",
+        number: "0254",
+        img: "./assets/images/collections/marmorette/articles/0254.jpeg",
+      },
+    ];
+  }
+  
+  function MarmoretteTable() {
+    return [
+      {
+        name: "Ширина рулона (м.пог.), EN-ISO 24341",
+        value: "2,0",
+        key: 'width',
+      },
+      {
+        name: "Длина рулона (м.пог.), EN-ISO 24341",
+        value: "20,0 - 31,0",
+        key: 'length',
+      },
+      {
+        name: "Толщина покрытия общая (мм.), EN-ISO 24346",
+        value: "2,0 / 2,5 / 3,2",
+        key: 'common-thikness',
+      },
+      {
+        name: "Вес покрытия (кг./кв.м.), EN-ISO 23997",
+        value: "2,4 / 2,9 / 3,8",
+        key: 'common-weight',
+      },
+      {
+        name: "Дополнительное защитное покрытие",
+        value: "Neocare",
+        key: 'extra-armor',
+      },
+      {
+        name: "Звукопоглощение (дБ), EN-ISO 717-2",
+        value: "3 дБ / 5 дБ / 6 дБ",
+      },
+      {
+        name: "Класс применения, EN-ISO 10874",
+        value: "23 / 34 / 43</br>23 / 32 / 41 в 2,0 мм",
+        key: 'class',
+      },
+      {
+        name: "Уровень противоскольжения, DIN 51130",
+        value: "R9",
+      },
+      {
+        name: "Наличие антибактериальных свойств",
+        value:
+          "> 99,99% бактерий гибнут через 24 часа",
+      },
+      {
+        name: "Устойчивость к химическим реактивам, EN-ISO 26987",
+        value:
+          "Есть",
+      },
+      {
+        name: "Устойчивость к следам от роликов мебели, EN 425",
+        value: "Есть",
+      },
+      {
+        name: "Цветостойкость, EN-ISO 105-B02",
+        value: "≥ 6",
+      },
+      {
+        name: "Препятствует развитию вирусов",
+        value: "Есть, в том числе covid-19",
+      },
+      {
+        name: "Класс пожарной опасности, EN 13501-1",
+        value: "КМ1",
+        key: 'km',
+      },
+      {
+          name: "Эко-сертификат",
+          value: "Cradle to cradle (Серебряный уровень)",
+      }
+    ];
+  }
+  
