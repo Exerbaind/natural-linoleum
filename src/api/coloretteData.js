f14f0e7a8af136ac4a7a1c76bea4f69a42859da8
Exerbaind <muchapm@yandex.ru>
2022-02-21 14:41:38 +0300

    new colorette collection

---
+++ src/api/coloretteData.js
@@ -0,0 +1,193 @@
+function ColoretteArticles() {
+    return [
+      {
+        id: 0,
+        name: "Banana Yellow",
+        number: "0001",
+        img: "./assets/images/collections/colorette/articles/0001.jpeg",
+      },
+      {
+        id: 1,
+        name: "Bluebird",
+        number: "0004",
+        img: "./assets/images/collections/colorette/articles/0004.jpeg",
+      },
+      {
+        id: 2,
+        name: "Vivid Green",
+        number: "0006",
+        img: "./assets/images/collections/colorette/articles/0006.jpeg",
+      },
+      {
+        id: 3,
+        name: "Light Beige",
+        number: "0012",
+        img: "./assets/images/collections/colorette/articles/0012.jpeg",
+      },
+      {
+        id: 4,
+        name: "Deep Orange",
+        number: "0016",
+        img: "./assets/images/collections/colorette/articles/0016.jpeg",
+      },
+      {
+        id: 5,
+        name: "Light Mud",
+        number: "0043",
+        img: "./assets/images/collections/colorette/articles/0043.jpeg",
+      },
+      {
+        id: 6,
+        name: "Oxid Grey",
+        number: "0052",
+        img: "./assets/images/collections/colorette/articles/0052.jpeg",
+      },
+      {
+        id: 7,
+        name: "Aluminium Grey",
+        number: "0058",
+        img: "./assets/images/collections/colorette/articles/0058.jpeg",
+      },
+      {
+        id: 8,
+        name: "Stone Grey",
+        number: "0059",
+        img: "./assets/images/collections/colorette/articles/0059.jpeg",
+      },
+      {
+        id: 9,
+        name: "Straw Beige",
+        number: "0071",
+        img: "./assets/images/collections/colorette/articles/0071.jpeg",
+      },
+      {
+        id: 10,
+        name: "Sand Yellow",
+        number: "0073",
+        img: "./assets/images/collections/colorette/articles/0073.jpeg",
+      },
+      {
+        id: 11,
+        name: "Elephant Grey",
+        number: "0080",
+        img: "./assets/images/collections/colorette/articles/0080.jpeg",
+      },
+      {
+        id: 12,
+        name: "Private Black",
+        number: "0081",
+        img: "./assets/images/collections/colorette/articles/0081.jpeg",
+      },
+      {
+        id: 13,
+        name: "Cadillac Pink",
+        number: "0110",
+        img: "./assets/images/collections/colorette/articles/0110.jpeg",
+      },
+      {
+        id: 14,
+        name: "Power Red",
+        number: "0118",
+        img: "./assets/images/collections/colorette/articles/0118.jpeg",
+      },
+      {
+        id: 15,
+        name: "Poppy Blue",
+        number: "0123",
+        img: "./assets/images/collections/colorette/articles/0123.jpeg",
+      },
+      {
+        id: 16,
+        name: "Spicy Green",
+        number: "0132",
+        img: "./assets/images/collections/colorette/articles/0132.jpeg",
+      },
+      {
+        id: 17,
+        name: "Kumquat Orange",
+        number: "0170",
+        img: "./assets/images/collections/colorette/articles/0170.jpeg",
+      },
+      {
+        id: 18,
+        name: "Sunrise Orange",
+        number: "0171",
+        img: "./assets/images/collections/colorette/articles/0171.jpeg",
+      },
+    ];
+  }
+  
+  function ColoretteTable() {
+    return [
+      {
+        name: "Ширина рулона (м.пог.), EN-ISO 24341",
+        value: "2,0",
+        key: 'width',
+      },
+      {
+        name: "Длина рулона (м.пог.), EN-ISO 24341",
+        value: "20,0 - 31,0",
+        key: 'length',
+      },
+      {
+        name: "Толщина покрытия общая (мм.), EN-ISO 24346",
+        value: "2,5",
+        key: 'common-thikness',
+      },
+      {
+        name: "Вес покрытия (кг./кв.м.), EN-ISO 23997",
+        value: "2,9",
+        key: 'common-weight',
+      },
+      {
+        name: "Дополнительное защитное покрытие",
+        value: "Neocare",
+        key: 'extra-armor',
+      },
+      {
+        name: "Звукопоглощение (дБ), EN-ISO 717-2",
+        value: " 5 дБ",
+      },
+      {
+        name: "Класс применения, EN-ISO 10874",
+        value: "23 / 34 / 43",
+        key: 'class',
+      },
+      {
+        name: "Уровень противоскольжения, DIN 51130",
+        value: "R9",
+      },
+      {
+        name: "Наличие антибактериальных свойств",
+        value:
+          "> 99,99% бактерий гибнут через 24 часа",
+      },
+      {
+        name: "Устойчивость к химическим реактивам, EN-ISO 26987",
+        value:
+          "Есть",
+      },
+      {
+        name: "Устойчивость к следам от роликов мебели, EN 425",
+        value: "Есть",
+      },
+      {
+        name: "Цветостойкость, EN-ISO 105-B02",
+        value: "≥ 6",
+      },
+      {
+        name: "Препятствует развитию вирусов",
+        value: "Есть, в том числе covid-19",
+      },
+      {
+        name: "Класс пожарной опасности, EN 13501-1",
+        value: "КМ1",
+        key: 'km',
+      },
+      {
+          name: "Эко-сертификат",
+          value: "Cradle to cradle (Серебряный уровень)",
+      }
+    ];
+  }
+  
