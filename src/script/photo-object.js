dd58ea75561524164dfee25ce3093008b46b4853
Exerbaind <muchapm@yandex.ru>
2022-05-10 20:19:48 +0300

    feat: slider dots

--- src/script/photo-object.js
+++ src/script/photo-object.js
@@ -1,24 +1,29 @@
 const prevSlide = document.getElementById("prev-slide");
 const nextSlide = document.getElementById("next-slide");
 const slides = [...document.querySelectorAll(".photos-slider__image")];
+const dots = [...document.querySelectorAll(".photos-slider__dot")];
 let activeSlideIndex = 0;
 
 nextSlide.onclick = function () {
   slides[activeSlideIndex].classList.remove("photos-slider__image--active");
+  dots[activeSlideIndex].classList.remove("photos-slider__dot--active");
   if (activeSlideIndex === slides.length - 1) {
     activeSlideIndex = 0;
   } else {
     activeSlideIndex += 1;
   }
   slides[activeSlideIndex].classList.add("photos-slider__image--active");
+  dots[activeSlideIndex].classList.add("photos-slider__dot--active");
 };
 
 prevSlide.onclick = function () {
   slides[activeSlideIndex].classList.remove("photos-slider__image--active");
+  dots[activeSlideIndex].classList.remove("photos-slider__dot--active");
   if (activeSlideIndex === 0) {
     activeSlideIndex = slides.length - 1;
   } else {
     activeSlideIndex -= 1;
   }
   slides[activeSlideIndex].classList.add("photos-slider__image--active");
+  dots[activeSlideIndex].classList.add("photos-slider__dot--active");
 };
